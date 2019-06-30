66d3c2fa1cc65fa2131e11633da1359f98bbb822
Diogo <diogo_pontes_95@hotmail.com>
2019-06-30 18:47:08 -0300

    adicionado o processamento de EQU e IF

--- tradutor/headers/preProcessa.hpp
+++ tradutor/headers/preProcessa.hpp
@@ -1,5 +1,7 @@
 #include "func.hpp"
 // retira comentarios, espacos e tabs
 void preProcessa(std::string saida, std::ifstream &arqEntrada);
+// processa as diretivas EQU e IF
+void preProcessa2(std::string saida);
 
 
--- tradutor/preProcessa.cpp
+++ tradutor/preProcessa.cpp
@@ -39,8 +39,7 @@ void preProcessa(std::string saida, std::ifstream &arqEntrada){
         linha_aux = linha;
         concat = 1;
       }
-    }
-    
+    }    
     //Escreve a linha se não for uma linha vazia
     if((int)linha[0] != 0){
       if(concat == 1){
@@ -56,6 +55,81 @@ void preProcessa(std::string saida, std::ifstream &arqEntrada){
       }
     }
   }
-
   arqSaida.close();
+  preProcessa2(saida);
+}
+
+void preProcessa2(std::string saida){
+  std::fstream arqSaida;
+  std::string str, operacao, rotulo;
+  size_t pos = 0;
+  std::vector<std::string> linhas;
+
+  arqSaida.open(saida.c_str());
+  int linha;
+
+  //armazena cada linha do arquivo em um vetor de strings
+  while(getline(arqSaida, str)){
+    linhas.push_back(str);
+  }
+  arqSaida.close();
+
+  for(linha = 0; linha <= (int)linhas.size()-1; linha++){
+    /*
+    PROCURA INSTRUCOES
+    */
+    //separa uma operacao que contem um label antes
+    if(linhas[linha].find(":") != std::string::npos){
+      pos = linhas[linha].find(":");
+      operacao = linhas[linha];
+      operacao.erase(0, pos+2);
+      pos = operacao.find(" ");
+      operacao = operacao.substr(0,pos);
+      
+    }
+    //separa uma operacao que nao contem um label antes
+    else{
+      pos = linhas[linha].find(" ");
+      operacao = linhas[linha].substr(0,pos);
+    }
+    if(operacao == "EQU"){
+        //std::cout << "Achou diretiva " << operacao << " na linha " << linha+1 << " endereco " << endereco << std::endl;
+        pos = linhas[linha].find(":");
+        rotulo = linhas[linha].substr(0,pos);
+        std::string equ = linhas[linha].substr(linhas[linha].find("EQU ")+4, 1); //equ so de um char por enquanto. ultimo argumento
+        linhas.erase(linhas.begin()+linha); //apaga a linha do equ
+        for(int j=linha; j<=(int)linhas.size()-1; j++){ //substitui equ em todas as outras linhas
+          if(linhas[j].find(rotulo) != std::string::npos){
+            linhas[j] = replaceAll(linhas[j], rotulo, equ);
+          }
+        }
+        linha--;
+        //endereco = endereco + 0;
+      }
+      else if(operacao == "IF"){
+        //std::cout << "Achou diretiva " << operacao << " na linha " << linha+1 << " endereco " << endereco << std::endl;
+        pos = linhas[linha].find(" ");
+        int valor_if = std::stoi(linhas[linha].substr(pos+1,pos+2));
+        //se o valor de if for 1, apaga a linha do if e deixa a proxima linha
+        if(valor_if == 1){
+          linhas.erase(linhas.begin()+linha);
+          linha--;
+        }
+        //se o valor de if for diferente de 1, apaga a linha do if e a proxima
+        else{
+          linhas.erase(linhas.begin()+linha);
+          linhas.erase(linhas.begin()+linha);
+          linha--;
+        }
+        //endereco = endereco + 0;
+      }
+  }
+
+  //grava o arquivo pre processado
+  std::ofstream arq2;
+  arq2.open(saida.c_str(), std::ofstream::trunc);
+  std::ostream_iterator<std::string> output_iterator(arq2, "\n");
+  std::copy(linhas.begin(), linhas.end(), output_iterator);
+  arq2.close();
+
 }
